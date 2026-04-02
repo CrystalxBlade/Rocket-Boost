d584d5e3b234276b5a0d52543ffbedef97f98ab3
CrystalxBlade <satish141m@gmail.com>
2026-04-03 00:02:59 +0530

    thrust

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,4 +4,16 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     [SerializeField] InputAction thrust;
+
+    private void OnEnable()
+    {
+        thrust.Enable();
+    }
+    void Update()
+    {
+        if(thrust.IsPressed())
+        {
+            Debug.Log("Thrust pressed");
+        }
+    }
 }
