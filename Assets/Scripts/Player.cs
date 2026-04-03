8c8e8554e162f299742462667a73d0edfa7a1216
CrystalxBlade <satish141m@gmail.com>
2026-04-03 20:35:20 +0530

    rigidBody

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,11 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     [SerializeField] InputAction thrust;
-
+    Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
     private void OnEnable()
     {
         thrust.Enable();
