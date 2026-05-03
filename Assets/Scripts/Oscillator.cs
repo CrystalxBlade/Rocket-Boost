9bd47d9947c7a5fe4100704b4e384fc9f983173c
CrystalxBlade <satish141m@gmail.com>
2026-05-03 22:12:53 +0530

    pingPong

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -2,13 +2,19 @@ using UnityEngine;
 
 public class Oscillator : MonoBehaviour
 {
-    
+    [SerializeField] Vector3 movementVector;
+    [SerializeField] float speed;
+    Vector3 startPos;
+    Vector3 endPos; 
+    float movementfactor;
     void Start()
     {
-        
+        startPos = transform.position;
+        endPos = startPos + movementVector;
     }
     void Update()
     {
         
+        transform.position = Vector3.Lerp(startPos, endPos, movementfactor);
     }
 }
