8dc148a88e01a5e57c0954b745bdd4cb195619ad
CrystalxBlade <satish141m@gmail.com>
2026-05-15 20:57:15 +0530

    soureces

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -2,5 +2,8 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    
+    [Header("--------------Audio Source--------------")]
+
+    [SerializeField] private AudioSource musicSource;  
+    [SerializeField] private AudioSource sfxSource;  
 }
