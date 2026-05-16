a311921de5f001100ac58c5cd3c4eaddc15f5117
CrystalxBlade <satish141m@gmail.com>
2026-05-16 18:56:40 +0530

    volumeManger

--- Assets/Scripts/VolumeManager.cs
+++ Assets/Scripts/VolumeManager.cs
@@ -5,14 +5,29 @@ using UnityEngine.Audio;
 public class VolumeManager : MonoBehaviour
 {
     public Slider musicSlider;
-    public AudioMixer audioMixer;
-    
+    public Slider sfxSlider;
+    public AudioMixer myMixer;
+
     void Start()
     {
-        
+        float savedVolume = PlayerPrefs.GetFloat("musicVolume", 1f);
+        musicSlider.value = savedVolume;
+        SetMusicVolume();
+
+        float savedSound = PlayerPrefs.GetFloat("sfxVolume", 1f);
+        sfxSlider.value = savedSound;
+        SetSFXVolume();
     }
-    void Update()
+    public void SetMusicVolume()
     {
-        
+        float volume = musicSlider.value;
+        myMixer.SetFloat("music", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("musicVolume", volume);
+    }
+    public void SetSFXVolume()
+    {
+        float sound = sfxSlider.value;
+        myMixer.SetFloat("sfx", Mathf.Log10(sound) * 20);
+        PlayerPrefs.SetFloat("sfxVolume", sound);
     }
 }
