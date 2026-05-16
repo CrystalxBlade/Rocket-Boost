6cc732af9ee53253b8e04256f1e5832dc5180b5a
CrystalxBlade <satish141m@gmail.com>
2026-05-16 13:26:10 +0530

    volumeManager

---
+++ Assets/Scripts/VolumeManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class VolumeManager : MonoBehaviour
+{
+    public Slider musicSlider;
+    public AudioMixer audioMixer;
+    
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
